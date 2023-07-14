42fdfcc2f0f13e459f7bd0a26492f5384870de1b
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-07-14 23:44:36 +0000

    tables_&_columns_structured

--- src/models.py
+++ src/models.py
@@ -7,23 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250))
+    e_mail = Column(String(250))
+    address = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Account(Base):
+    __tablename__ = 'account'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey('user.id'))
+    username= Column(String(250))
+    password = Column(String(250))
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey('user_id.id'))
+    character = Column(String(250))
+    planets = Column(String(250))
+    vehicles = Column(String(250))
+    id = Column(Integer, primary_key=True)
+
+class Rating(Base):
+    __tablename__ = 'rating'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, ForeignKey('user_id.id'))
+    rating_id = Column(String(250))
+    post_id = Column(String(250))
+    rating_value = Column(String(250))
+    id = Column(Integer, primary_key=True)
 
     def to_dict(self):
         return {}
